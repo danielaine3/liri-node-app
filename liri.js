ab2e97ad9e35f1a59c96964fc32bd2da0105803b
Dani Carter <carte470@d.umn.edu>
2018-02-06 20:41:01 -0600

    starting api calls

--- liri.js
+++ liri.js
@@ -9,29 +9,78 @@ var Twitter = require('client');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-request("http://www.omdbapi.com/?t=" + media + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
-	if (!error && response.statusCode === 200) {
-		console.log("Request complete.");
-	};
-});
-
-
 var command = process.argv[2];
-var media = process.argv[3];
+switch (command) {
+	case 'my-tweets':
+		getTweets();
+		break;
 
-if (command === "my-tweets") {
-	var queryURL=""
-}
+	case "spotify-this-song":
+		var songName = process.argv[3];
+		spotifyThis(songName);
+		break;
 
+	case 'movie-this':
+		var movieName = process.argv[3];
+		movieThis(movieName);
+		break;
 
-else if (command === "spotify-this-song") {
-	var 
-}
+	case 'do-what-it-says':
+		doWhatItSays();
+		break;
+	default:
+		console.log("LIRI doesn't know that");
+};
 
-else if (command === "movie-this") {
+function getTweets() {
 
-}
+};
 
-else if (command === "do-what-it-says") {
+function spotifyThis(songName) {
+	if (songName === " ") {
+		songName = "Revolution"
+	};
 
+	var queryURL = 'http://api.spotify.com/v1/search/q=track'+songName
+
+	console.log(response)};
+
+	var spotifyResponse = JSON.parse(body);
+	console.log("Artist(s): " +);
+	console.log("Song Name: " +);
+	console.log("Spotify Preview: " +);
+	console.log("Album: " + )
+};
+
+function movieThis(title) {
+	if (movieName == " ") {
+		movieName = 'Mr. Nobody';
+	};
+	var queryURL = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey=trilogy';
+
+	request(queryURL, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			console.log("-----------------------")
+			console.log("Request complete.");
+
+		
+			var movieResponse = JSON.parse(body);
+			console.log("Title: " + movieResponse.Title);
+			console.log("Year released: " + movieResponse.Year);
+			console.log("IMDB rating: " + movieResponse.imdbRating);
+			console.log("Rotten Tomatoes Rating: " + movieResponse.Ratings);
+			console.log("Production country: " + movieResponse.Country);
+			console.log("Language: " + movieResponse.Language);
+			console.log("Plot: " + movieResponse.Plot);
+			console.log("Actors: " + movieResponse.Actors);
+		};	
+	});
+};
+
+function doWhatItSays() {
+	fs.readFile("random.txt", "utf8", function(error, data) {
+		console.log(data);
+		writeToLog(data);
+		// var dataArr = data.split(',');
+	})
 }
